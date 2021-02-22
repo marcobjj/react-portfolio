da66c163c6ae92c54225f897ed71b46ef000a0b4
Marco Evangelista <marcobjj@users.noreply.github.com>
2021-02-22 13:49:12 -0800

    changes to about and resume sections

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -8,7 +8,14 @@ function About() {
     <h1 id="about">Who am I?</h1>
     
      <img src={require(`../../assets/images/self.jpg`).default} className="my-2" width="574" height="360" alt="cover" />
-  <p> Brazilian born and raised, currently living in Los Angeles. <br></br>I am a Graphic Design graduate for UFSC and currently a <br></br> UC Berkeley Coding Bootcamp student.</p>
+  <p>Full Stack Developer (MERN)</p>
+
+  <p>Coding Bootcamp Graduate from UC Berkeley</p>
+  <p>Bachelor’s Degree in Graphic Design</p>
+  <p>Greater Los Angeles Area.</p>
+
+
+  <p>I started out as Adobe Flash and Actionscript developer, which I did for 6 years. Due to the similarities between Actionscript and Javascript I was able to transition to Javascript based development quickly. I'm generalist (Full stack) developer and work both front-end / UX development, but have participated in and enjoy backend dev work just as much. </p>
    </div>
   </section>
   
